18a07e2b368b5ba4832f012b8dec86072c53172b
QuangNguyen <nguyenquangos0302app@gmail.com>
2023-05-07 18:18:55 +0700

    upload

---
+++ src/main/java/com/example/demo/api/HelloApi.java
@@ -0,0 +1,16 @@
+package com.example.demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloApi {
+
+    @GetMapping("")
+    public String hello() {
+        return "Hello World";
+    }
+
+}
